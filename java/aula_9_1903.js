7fd31ef53ac0c8b8c5008802934a2cf09ec2b8b5
cleibersonfreitas <cleiberson.freitas@gmail.com>
2025-03-24 21:44:06 -0300

    Add files via upload

---
+++ java/aula_9_1903.js
@@ -0,0 +1,47 @@
+//////////////////////////////////////////////////////////////
+// VETORES / ARRAYS em JavaScript - Aula 9                 ///
+/////////////////////////////////////////////////////////////
+// Arrays em JavaScript são estruturas de dados 
+
+
+
+
+
+
+
+
+// push: Adicione
+
+frutas.push("Laranja");
+console.log(frutas);
+
+// count: Em
+
+console.log(frutas.length);
+
+//copy:Para copiar um array, podemos usar o método slice().
+
+let copiaFrutas = frutas.slice();
+console.log(copiaFrutas);
+
+// random: Para selecionar um elemento aleatório de um 
+
+let randomIndex = Match.floor(Math.random() *frutas)
+console.log(frutas[randomIndex]);
+
+//range:JavaScript não tem uma função range nativa, mas podemos criar uma.
+
+function range(start,end) {
+     return Array.front
+
+
+ console.log(range(1, 5));
+
+
+
+ let num= [3, 1, 4, 1, 5, 9];
+ num.sort((a, b) =>a - b);
+
+ // adicionar um novo número 
+numeros.push(7);
+console.log("após push:, numeros);
